f8e1f554250dfed55cd72a3687b1b19bd89c4cac
Bruce LeBlanc <leblanc_bruce@hotmail.com>
2014-07-02 20:24:54 -0300

    updated comment on servlet context listener

--- Tombstone/server/src/main/java/com/tombstone/server/context/listener/TombstoneServletContextListener.java
+++ Tombstone/server/src/main/java/com/tombstone/server/context/listener/TombstoneServletContextListener.java
@@ -1,30 +1,45 @@
 package com.tombstone.server.context.listener;
 
-import javax.faces.bean.ManagedProperty;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import com.lebcool.common.logging.Logger;
-import com.tombstone.server.bean.ApplicationBean;
 
+/**
+ * <p>
+ * The servlet context listener for the Tombstone application.
+ * </p>
+ */
 public final class TombstoneServletContextListener
     implements ServletContextListener
 {
     //:: ---------------------------------------------------------------------
     //:: Public Interface
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void contextInitialized(final ServletContextEvent event)
     {
+        // Simply log the fact that the context was initialized.
         LOGGER.info(this, "The servlet context was initialized.");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void contextDestroyed(final ServletContextEvent event)
     {
+        // Simply log the fact that the context was destroyed.
         LOGGER.info(this, "The servlet context was destroyed.");
     }
 
+    /**
+     * @return A string representation of this class used for logging
+     *         purposes.
+     */
     @Override
     public String toString()
     {
@@ -32,14 +47,9 @@ public final class TombstoneServletContextListener
     }
 
     //:: ---------------------------------------------------------------------
-    //:: Private Data Members
-
-    @ManagedProperty(value="#{applicationBean}")
-    private ApplicationBean _applicationBean;
-
-    //:: ---------------------------------------------------------------------
     //:: Private Constants
 
+    // The logger instance used to emit log messages to the application log.
     private static final Logger LOGGER
         = new Logger(TombstoneServletContextListener.class);
 }
